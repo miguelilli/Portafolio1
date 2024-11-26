0670b112f1f8a38f42ddad8a800e38a4c09fd699
miguelilli <149540214+miguelilli@users.noreply.github.com>
2024-11-26 16:44:57 -0500

    Add files via upload

---
+++ restaurante.js
@@ -0,0 +1,11 @@
+//crear factura de restaurante/miguel palacio
+var totalcomidad = 2800 + 1300;
+var totalbebidad = 900 + 650;
+var total = totalcomidad + totalbebidad;
+
+console.log("El total de comidad es: " + totalcomidad);
+console.log("El total de bebidad es: " + totalbebidad);
+console.log("El total de cuenta es: " + total);
+
+
+
